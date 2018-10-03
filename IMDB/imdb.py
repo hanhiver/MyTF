e2869497a3d9addc9b1e8735cce6edb3f432cae1
hanhiver <13910395157@139.com>
2018-10-03 13:52:47 +0800

    update 2018.10.03
    Add the IMDB.py files.

--- IMDB/imdb.py
+++ IMDB/imdb.py
@@ -15,12 +15,29 @@ keras.backend.set_session(sess)
 import numpy as np 
 
 print(tf.__version__)
-print("Test")
 
-"""
+
 # Download the IMDB data.
 imdb = keras.datasets.imdb
-(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words = 100)
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words = 10000)
 
 print("Training entries: {}, labels: {}".format(len(train_data), len(train_labels)))
-"""
+
+# A dictionary mapping words to an integer index. 
+word_index = imdb.get_word_index()
+
+# The first indices are reserved.
+word_index = {k:(v + 3) for k, v in word_index()}
+word_index["<PAD>"] = 0
+word_index["<START"] = 1
+word_index["<UNK>"] = 2
+word_index["<UNUSED>"] = 3
+
+
+reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+
+def decode_review(text):
+	return ' '.join([reverse_word_index.get(i, '?') for i in text])
+
+decode_review(train_data[0])
+
