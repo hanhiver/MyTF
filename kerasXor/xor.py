65b4b7f0ce99d7a0802dbb1b5bdf53eca1755484
hanhiver <13910395157@139.com>
2018-10-14 17:08:45 +0800

    update 2018.10.15

--- kerasXor/xor.py
+++ kerasXor/xor.py
@@ -2,6 +2,16 @@ import os
 import tensorflow as tf  
 from tensorflow import keras
 
+# Set the GPU environment. 
+# Use 50% gpu memory. 
+# import tensorflow.keras.backend.tensorflow_backend as ktf
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+config = tf.ConfigProto(log_device_placement = True)
+config.gpu_options.per_process_gpu_memory_fraction = 0.5
+config.gpu_options.allow_growth = True
+sess = tf.Session(config = config)
+keras.backend.set_session(sess)
+
 import numpy as np 
 from matplotlib import pyplot as plt 
 
