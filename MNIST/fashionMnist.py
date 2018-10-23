25b2a7c4cd70abcf29652080823affb76eee854f
hanhiver <13910395157@139.com>
2018-10-23 03:15:17 -0400

    Update 2018.10.23
    Run some test program in the dliforsup host.

--- MNIST/fashionMnist.py
+++ MNIST/fashionMnist.py
@@ -8,8 +8,8 @@ from tensorflow import keras
 # Use 50% gpu memory. 
 # import tensorflow.keras.backend.tensorflow_backend as ktf
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
-config = tf.ConfigProto(log_device_placement = False)
-config.gpu_options.per_process_gpu_memory_fraction = 0.5
+config = tf.ConfigProto(log_device_placement = True)
+config.gpu_options.per_process_gpu_memory_fraction = 0.8
 config.gpu_options.allow_growth = True
 sess = tf.Session(config = config)
 keras.backend.set_session(sess)
@@ -107,7 +107,7 @@ cp_callback = tf.keras.callbacks.ModelCheckpoint(
 	)
 
 # Train the model.
-model.fit(train_images, train_labels, epochs = 1,
+model.fit(train_images, train_labels, epochs = 5,
 	validation_data = (test_images, test_labels),
 	callbacks = [cp_callback]
 	)
