373541d56a3a17dcae12466fec282e1426690c11
Dong Han <handongfr@gmail.com>
2018-10-30 14:40:28 +0800

    Update capsNetMNIST/capsNet-mnist.py

--- capsNetMNIST/capsNet-mnist.py
+++ capsNetMNIST/capsNet-mnist.py
@@ -261,7 +261,7 @@ saver = tf.train.Saver()
 
 # 训练模型
 n_epochs = 3
-batch_size = 200
+batch_size = 250
 restore_checkpoint = False
 
 n_iterations_per_epoch = mnist.train.num_examples // batch_size
