50666c64a58da09d46d10bb7e1052803a8256597
Dong Han <handongfr@gmail.com>
2018-11-07 16:44:58 +0800

    update 2018.11.07

--- MNIST/fashionMnist.py
+++ MNIST/fashionMnist.py
@@ -8,7 +8,7 @@ from tensorflow import keras
 # Use 50% gpu memory. 
 # import tensorflow.keras.backend.tensorflow_backend as ktf
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
-config = tf.ConfigProto(log_device_placement = True)
+config = tf.ConfigProto(log_device_placement = False)
 config.gpu_options.per_process_gpu_memory_fraction = 0.8
 config.gpu_options.allow_growth = True
 sess = tf.Session(config = config)
